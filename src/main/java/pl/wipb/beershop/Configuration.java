969e5fecd4a229feb388521fa8436b4c72437189
unknown <bart.karolczak@gmail.com>
2024-06-06 11:38:36 +0200

    Changed JPA config a bit

--- src/main/java/pl/wipb/beershop/Configuration.java
+++ src/main/java/pl/wipb/beershop/Configuration.java
@@ -2,16 +2,18 @@ package pl.wipb.beershop;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
 
 @DataSourceDefinition(
-        name = "java:global/BeerShopDataSource",
+        name = "java:global/H2DB",
         className = "org.h2.jdbcx.JdbcDataSource",
         url = "jdbc:h2:mem:db;DB_CLOSE_DELAY=-1",
         minPoolSize = 1,
         initialPoolSize = 1,
-        user = "dbuser",
-        password = "dbuser123"
+        user = "sa",
+        password = "sa"
 )
 @Singleton
+@Startup
 public class Configuration {
 }
