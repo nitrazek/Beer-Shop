8e12378fb4c91723dab8b8a7fd77f28df36ad942
unknown <bart.karolczak@gmail.com>
2024-06-13 19:18:32 +0200

    Added MailService class

---
+++ src/main/java/pl/wipb/beershop/services/MailService.java
@@ -0,0 +1,12 @@
+package pl.wipb.beershop.services;
+
+import jakarta.ejb.Asynchronous;
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class MailService {
+  @Asynchronous
+  public void sendMail() {
+    
+  }
+}
