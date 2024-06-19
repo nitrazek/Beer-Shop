5d2add9c94dd834cb9ab70a4bef3e2c07f29fb0b
xShyQx <93669491+xShyQx@users.noreply.github.com>
2024-06-19 15:30:33 +0200

    Added more categories to ProductCategory

--- src/main/java/pl/wipb/beershop/models/utils/ProductCategory.java
+++ src/main/java/pl/wipb/beershop/models/utils/ProductCategory.java
@@ -1,5 +1,9 @@
 package pl.wipb.beershop.models.utils;
 
 public enum ProductCategory {
-    BEER;
+    BEER,
+    BEER_GLASSWARE,
+    HOMEBREWING_EQUIPMENT,
+    BEER_ACCESSORY,
+    BEER_MERCHANDISE;
 }
